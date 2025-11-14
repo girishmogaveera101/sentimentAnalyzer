5d1eeb300b828b482f37511189b1b1e8dfc2d0e9
Girish Mogaveera <girizhhh@gmail.com>
2025-11-15 00:09:24 +0530

    requiremnets.text upload2

--- server/main.py
+++ server/main.py
@@ -1,29 +1,17 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load variables from .env
 
 app = FastAPI()
 
-model_id = "girizhhh/sentimentAnalyzer"
+API_URL = "https://api-inference.huggingface.co/models/girizhhh/sentimentAnalyzer"
+headers = {"Authorization": f"Bearer {os.getenv('HF_TOKEN')}"}
 
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForSequenceClassification.from_pretrained(model_id)
-
-class InputText(BaseModel):
-    text: str
-
-@app.post("/predict")
-def predict(input: InputText):
-    inputs = tokenizer(input.text, return_tensors="pt")
-    outputs = model(**inputs)
-    logits = outputs.logits
-    probs = torch.softmax(logits, dim=1)
-    pred = torch.argmax(probs).item()
-    print("Logits : ",float(probs[0][0]),float(probs[0][1]))
-    # label = "positive" if pred == 1 else "negative"
-    # confidence = float(probs[0][pred])
-    return {
-        "negative": float(probs[0][0]),
-        "positive": float(probs[0][1])
-    }
+@app.get("/predict")
+def predict(text: str):
+    payload = {"inputs": text}
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
